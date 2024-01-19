85078be754b6f01d273e4b8a7ded6e38eb923e51
davidinovargas <davidinovargas@gmail.com>
2024-01-19 08:45:06 -0300

    arquivos base

---
+++ src/main/java/equoterapia/equo/EquoApplication.java
@@ -0,0 +1,13 @@
+package equoterapia.equo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/equoterapia/equo/EquoApplicationTests.java
@@ -0,0 +1,13 @@
+package equoterapia.equo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

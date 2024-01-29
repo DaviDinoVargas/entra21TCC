f0b2bca447bafed7e1cec534c9fd44f515e05db9
davidinovargas <davidinovargas@gmail.com>
2024-01-29 09:10:19 -0300

    Correção SecurityConfiguration

--- src/main/java/config/SecurityConfiguration.java
+++ src/main/java/config/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration {
 			  .csrf(csrf -> csrf.disable())
 			  .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			  .authorizeHttpRequests(authorize -> authorize
-					  .requestMatchers(HttpMethod.GET,"/consultas", "/Paciente", "/Enderecos", "/TBEscala", "/TBEscala2", "/TBEscala3").permitAll()
+					  .requestMatchers(HttpMethod.GET,"/consultas", "/Paciente", "/Enderecos", "/TBEscala1", "/TBEscala2", "/TBEscala3").permitAll()
 					  .requestMatchers(HttpMethod.GET,"/Usuario").hasRole("ADMIN")
 					  .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
 					  .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()

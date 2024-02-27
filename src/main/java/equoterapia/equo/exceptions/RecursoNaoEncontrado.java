4d0b22e3a028b85720d65fc1d4044f78cb8cbd62
Davi <139391138+DaviDinoVargas@users.noreply.github.com>
2024-02-27 08:50:50 -0300

    Delete src directory

--- src/main/java/equoterapia/equo/exceptions/RecursoNaoEncontrado.java
+++
@@ -1,9 +0,0 @@
-package equoterapia.equo.exceptions;
-
-public class RecursoNaoEncontrado extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public RecursoNaoEncontrado(String msg) {
-		super(msg);
-	}
-}
--- src/main/java/equoterapia/equo/repositories/InstrutorRepository.java
+++
@@ -1,8 +0,0 @@
-package equoterapia.equo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import equoterapia.equo.entidades.Instrutor;
-
-public interface InstrutorRepository extends JpaRepository<Instrutor,Long>{
-
-}
